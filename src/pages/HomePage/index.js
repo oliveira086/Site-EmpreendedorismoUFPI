e7683b532528d5bd6c903631f77a8ce325ab29b8
André Oliveira <aoliveira@3xsolutions.com.br>
2020-09-18 23:45:32 -0300

    Fix: Adding header in home and change style in css for header component

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
+import Header from '../../components/Header'
 import Sectors from '../../components/Sectors';
 import EventsCarousel from '../../components/EventsCarousel';
 import OurTeam from '../../components/OurTeam';
@@ -8,19 +9,8 @@ import OurTeam from '../../components/OurTeam';
 function HomePage() {
   return (
     <div>
-      <h2>Home</h2>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/sobre'>Quem Somos!</Link></li>
-        <li><Link to='/contato'>Contato</Link></li>
-        <li><Link to='/servicos'>Serviços</Link></li>
-        <li><Link to='/eventos'>Eventos</Link></li>
-        <li><Link to='/login'>Login</Link></li>
-        <li><Link to='/cadastro'>Cadastro</Link></li>
-        <li><Link to='/admin'>Painel do admin</Link></li>
-        <li><Link to='/usuario'>Painel do usuário</Link></li>
-      </ul>
 
+      <Header/>
       <Sectors />
 
       <EventsCarousel />
